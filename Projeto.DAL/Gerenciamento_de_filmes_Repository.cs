aa754ed0996a7e058bc2207abb8a9f8dd74725c4
marcio henrique <mhmuzy@embratel.com.br>
2021-11-19 18:52:10 -0300

    Merge

--- Projeto.DAL/Gerenciamento_de_filmes_Repository.cs
+++ Projeto.DAL/Gerenciamento_de_filmes_Repository.cs
@@ -64,7 +64,7 @@ namespace Projeto.DAL
         }
 
         public void Delete(Gerenciamento_de_filmes G)
-        /// Metodo de Desligamento do Fale Conosco da Aplicacao
+        /// Metodo de Desligamento do Lista_de_salas
         {
             OpenConnection();
             /// Abre a Conexao com a Base
