7bda7c59d66becfeffd3ddd5767c13d2f9a2242a
marcio henrique <mhmuzy@embratel.com.br>
2021-12-06 20:13:43 -0300

    Inicialização dos Testes Unitários

---
+++ Projeto.Apresentacao2.Tests/Controllers/FilmeControllerTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Projeto.Entidades;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using Projeto.Apresentacao2;
+
+namespace Projeto.Apresentacao2.Tests.Controllers
+{
+    [TestClass]
+    public class FilmeControllerTest
+    {
+        [TestMethod]
+        public void UnitTest1()
+        {
+            Gerenciamento_de_filmes gf = new Gerenciamento_de_filmes();
+            gf.Titulo = "Uma Noite no Museu 2";
+            gf.Imagem = "download.png";
+            gf.Duracao = 3;
+            gf.DuracaoMinuto = 30;
+            gf.DuracaoSegundo = 20;
+            gf.Descricao = "Filme Muito Bom Para Toda a Família";
+
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["GerenciadorDoCinema"].ConnectionString);
+            con.Open();
+
+            SqlCommand com = new SqlCommand("INSERT INTO Gerenciamento_de_filmes(Titulo, Descricao, Duracao, " +
+                    "Imagem, DuracaoMinuto, DuracaoSegundo) VALUES ('" + gf.Titulo + "', '" + gf.Descricao + "', " + gf.Duracao + ", '" + gf.Imagem + "', " + gf.DuracaoMinuto + ", " + gf.DuracaoSegundo + ")", con);
+            com.ExecuteReader();
+
+            
+
+            con.Close();
+        }
+    }
+}
--- Projeto.Apresentacao2.Tests/UnitTest1.cs
+++ Projeto.Apresentacao2.Tests/UnitTest1.cs
@@ -1,14 +1,43 @@
 ﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Projeto.Apresentacao2;
+using Projeto.DAL;
+using Projeto.Entidades;
 
 namespace Projeto.Apresentacao2.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTest1 : Conexao
     {
         [TestMethod]
         public void TestMethod1()
         {
+            try
+            {
+
+            
+            con = new SqlConnection(ConfigurationManager.ConnectionStrings["GerenciadorDoCinema"].ConnectionString);
+            con.Open();
+            Gerenciamento_de_filmes gf = new Gerenciamento_de_filmes();
+            gf.Titulo = "Deu a Louca nos bichos";
+            gf.Imagem = "download.png";
+            gf.Descricao = "Filme Para Toda a Família";
+            gf.Duracao = 1;
+            gf.DuracaoMinuto = 30;
+            gf.DuracaoSegundo = 45;
+
+            Gerenciamento_de_filmes_Repository rep = new Gerenciamento_de_filmes_Repository();
+
+            rep.Insert(gf);
+            con.Close();
+            }
+            catch (Exception)
+            {
+
+                Assert.IsTrue(true);
+            }
         }
     }
 }
